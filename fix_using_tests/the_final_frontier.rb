492aacf1f63597a2b7cdef991f1e6fc1767a077f
JT Lu <alohajt94@gmail.com>
2020-02-08 00:55:43 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -11,12 +14,18 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+def greet_crew(crew)
+  newCrew = []
+  crew.each {|crew_member| newCrew << "Hello #{crew_member}."}
+  return newCrew
+  #binding.pry
 end
 
 def engage
   date = generate_star_date
   state_log(date)
   greet_crew(crew)
+  #binding.pry
 end
+
+#engage
--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,17 +1,35 @@
-
+require 'pry'
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
 
 def turtle_traits(turtles)
-  turtles.map do |turtle|
-    turtle[:traits].each do |trait|
+  
+  arr = []
+  arr = turtles.map do |turtle|
+    turtle[:traits] do |trait|
       trait
     end
   end
+  binding.pry
+  return arr
+  
 end
+
+
+great thanks, this is waht im looking options_from_collection_for_select for
+
+#
+# turtle,map = turtle =       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
+#       {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
+#       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
+#       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
+
+# turtle[:traits] =["serious", "leader"], ["party dude"],  ["cool", "rude"], ["loves being a turtle"]
+
+turtle_traits(turtles)
